0eba9c72d962986348b5a07d56d4efcc62e96ad7
Matěj Hyška <matej.hyska.020@pslib.cz>
2021-12-21 12:40:43 +0100

    1st

--- Csharp_Collections/Csharp_Collections/Program.cs
+++ Csharp_Collections/Csharp_Collections/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Csharp_Collections
 {
@@ -6,7 +10,29 @@ namespace Csharp_Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> list = new List<Student>(3) { new Student("Adam"), new Student("Katka", 15)};
+            foreach (var item in list)
+            {
+                if (item.Age != 0)
+                {
+                    Console.WriteLine(item.Name + ": " + item.Age);
+                    
+                }
+                else Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("\n############################\n");
+
+            Console.WriteLine("Kapacita: " + list.Capacity);
+            Console.WriteLine("Počet: " + list.Count);
+            Console.WriteLine(list[0].Name);
+
+            Console.WriteLine("\n############################\n");
+
+            list.Clear();
+            list.Add(new Student("Lukáš", 24));
+            Console.WriteLine(list.Contains(list[0]));
+
+
         }
     }
 }
---
+++ Csharp_Collections/Csharp_Collections/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Collections
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Student(string name)
+        {
+            Name = name;
+        }
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
